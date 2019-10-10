96cbc3895ddc555303b606162dabc4e8ac4bca2e
Jazmin M <jazminmerckel@gmail.com>
2019-10-10 01:25:31 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
 ## code your solution here. 
+class Cat
+ 
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
